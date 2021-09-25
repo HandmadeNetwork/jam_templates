a6248f0836fe4552bc9808c548812ca132306570
ryanfleury <ryan.j.fleury@gmail.com>
2021-09-25 10:27:54 -0600

    initial upload of first 3 templates

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo off
+if not exist build mkdir build
+pushd build
+
+copy ..\libs\SDL2.dll .\
+copy ..\libs\raylib.dll .\
+
+rem ~~~ build SDL2/DearImGui/OpenGL3 template
+cl /nologo /Zi /I..\src\ ../src/template_sdl_dearimgui_opengl3.cpp ../src/third_party/dear_imgui/imgui*.cpp /link ..\libs\SDL2.lib ..\libs\SDL2main.lib opengl32.lib shell32.lib
+
+rem ~~~ build SDL2/MicroUI/OpenGL template
+cl /nologo /Zi /I..\src\ ../src/template_sdl_microui_opengl3.c ../src/third_party/microui/microui.c /link ..\libs\SDL2.lib ..\libs\SDL2main.lib opengl32.lib shell32.lib
+
+rem ~~~ build raylib template
+cl /nologo /Zi /I..\src\ ../src/template_raylib.c /link ..\libs\raylibdll.lib
+
+popd
